f637c765a5d00127ba39bc840c9bd901fe9490bf
SRNV <alula.rudy@gmail.com>
2020-08-25 17:24:55 +0200

    feat(wip) : wip

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const buildTmL = require("./build.js");
 buildTmL("./src/", process.cwd() + "/syntaxes/ogone.tmLanguage.json");
 buildTmL(
   "./injections/proto/",
-  process.cwd() + "/syntaxes/injection.proto.json"
+  process.cwd() + "/syntaxes/injection.proto.json",
+);
+buildTmL(
+  "./injections/style/",
+  process.cwd() + "/syntaxes/injection.style.json",
 );
 
 buildTmL(
   "./injections/flags/",
-  process.cwd() + "/syntaxes/injection.flag.json"
+  process.cwd() + "/syntaxes/injection.flag.json",
 );
 
 buildTmL(
   "./injections/templates/",
-  process.cwd() + "/syntaxes/injection.template.json"
+  process.cwd() + "/syntaxes/injection.template.json",
 );
