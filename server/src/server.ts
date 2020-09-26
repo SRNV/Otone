54c0ad7034efd5290d88e4a935f579a3a0103260
SRNV <alula.rudy@gmail.com>
2020-09-26 17:03:21 +0200

    hot publish

--- server/src/server.ts
+++ server/src/server.ts
@@ -148,7 +148,7 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
 		problems++;
 		let diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
+			severity: DiagnosticSeverity.Error,
 			range: {
 				start: textDocument.positionAt(m.index),
 				end: textDocument.positionAt(m.index + m[0].length)
@@ -156,23 +156,23 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 			message: `${m[0]} is all uppercase.`,
 			source: 'ex'
 		};
-		if (hasDiagnosticRelatedInformationCapability) {
-			diagnostic.relatedInformation = [
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Spelling matters'
+		diagnostic.relatedInformation = [
+			{
+				location: {
+					uri: textDocument.uri,
+					range: Object.assign({}, diagnostic.range)
 				},
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Particularly for names'
-				}
-			];
+				message: 'Spelling matters'
+			},
+			{
+				location: {
+					uri: textDocument.uri,
+					range: Object.assign({}, diagnostic.range)
+				},
+				message: 'Particularly for names'
+			}
+		];
+		if (hasDiagnosticRelatedInformationCapability) {
 		}
 		diagnostics.push(diagnostic);
 	}
