2ac3178e2f3d76de5a499c9bef06b23e7d59e80e
SRNV <alula.rudy@gmail.com>
2020-09-28 18:40:51 +0200

    hot publish

--- server/src/server.ts
+++ server/src/server.ts
@@ -131,6 +131,7 @@ documents.onDidClose(e => {
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
+	connection.console.warn(change.document.getText())
 	validateTextDocument(change.document);
 });
 
