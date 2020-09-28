a2e30181ea84047ba512a0abccd9bcb771aa2305
SRNV <alula.rudy@gmail.com>
2020-09-28 18:34:50 +0200

    hot publish

--- server/src/server.ts
+++ server/src/server.ts
@@ -139,40 +139,41 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 	let text = textDocument.getText();
 	let pattern = /([A-Z])+/g;
 	let m: RegExpExecArray | null;
-
-	let problems = 0;
 	let diagnostics: Diagnostic[] = [];
 	m = /([A-Z])+/g.exec(text);
-		problems++;
-		let diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Error,
-			range: {
-				start: textDocument.positionAt(m.index),
-				end: textDocument.positionAt(m.index + m[0].length)
-			},
-			message: `${m[0]} is all uppercase.`,
-			source: 'ex'
-		};
-		if (hasDiagnosticRelatedInformationCapability) {
-			diagnostic.relatedInformation = [
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Spelling matters'
+	if (m) {
+		m.forEach((input) => {
+			let diagnostic: Diagnostic = {
+				severity: DiagnosticSeverity.Error,
+				range: {
+					start: textDocument.positionAt(text.indexOf(input)),
+					end: textDocument.positionAt(text.indexOf(input) + input.length)
 				},
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
+				message: `${input} is all uppercase.`,
+				source: 'ex'
+			};
+			if (hasDiagnosticRelatedInformationCapability) {
+				diagnostic.relatedInformation = [
+					{
+						location: {
+							uri: textDocument.uri,
+							range: Object.assign({}, diagnostic.range)
+						},
+						message: 'Spelling matters'
 					},
-					message: 'Particularly for names'
-				}
-			];
-		}
-		diagnostics.push(diagnostic);
-	// Send the computed diagnostics to VSCode.
+					{
+						location: {
+							uri: textDocument.uri,
+							range: Object.assign({}, diagnostic.range)
+						},
+						message: 'Particularly for names'
+					}
+				];
+			}
+			diagnostics.push(diagnostic);
+		// Send the computed diagnostics to VSCode.
+		})
+	}
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
