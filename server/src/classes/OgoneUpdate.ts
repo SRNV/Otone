d9e0192ba9efd217cabb5961bdd60446059bf7b2
SRNV <alula.rudy@gmail.com>
2020-09-29 19:49:45 +0200

    hot publish

--- server/src/classes/OgoneUpdate.ts
+++ server/src/classes/OgoneUpdate.ts
@@ -19,12 +19,11 @@ import {
 
 export default class OgoneUpdate extends Collections {
   protected update(document: TextDocument) {
+    this.updateDocument(document);
+    this.inspectForbiddenTextnodes(document);
+  }
+  protected inspectForbiddenTextnodes(document: TextDocument) {
     const o3 = this.getItem(document.uri);
-    if (o3) {
-      o3.text = document.getText();
-      o3.nodes = this.render(o3.text);
-      o3.assets = this.getAssets(o3.nodes);
-    }
     if (o3) {
       const { nodes } = o3;
       const forbiddenTextNodes = nodes.filter((node, id) =>
@@ -33,18 +32,28 @@ export default class OgoneUpdate extends Collections {
         && (node as any).nodeValue.trim().length
       );
       forbiddenTextNodes.forEach((node) => {
-        this.sendDiagnostics(o3.document, [
-          {
-            severity: DiagnosticSeverity.Error,
-            message: `forbidden textnode: ${(node as any).nodeValue}`,
-            range: {
-              start: o3.document.positionAt(node.startIndex + 1),
-              end: o3.document.positionAt(node.endIndex + 1)
-            },
-            source: 'otone',
-          }
-        ]);
+        if ((node as any).nodeValue && (node as any).nodeValue.trim().length) {
+          this.sendDiagnostics(o3.document, [
+            {
+              severity: DiagnosticSeverity.Error,
+              message: `forbidden textnode: ${(node as any).nodeValue}`,
+              range: {
+                start: o3.document.positionAt(node.startIndex + 1),
+                end: o3.document.positionAt(node.endIndex + 1)
+              },
+              source: 'otone',
+            }
+          ]);
+        }
       });
+    }
+  }
+  protected updateDocument(document: TextDocument) {
+    const o3 = this.getItem(document.uri);
+    if (o3) {
+      o3.text = document.getText();
+      o3.nodes = this.render(o3.text);
+      o3.assets = this.getAssets(o3.nodes);
     }
   }
   protected sendDiagnostics(document: TextDocument, diagnostics: Diagnostic[]) {
